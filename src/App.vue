5bbc3731308e8dc5cb262e60c32635b4aad39f10
Andreas Omholt Olsen <andreaoo@stud.ntnu.no>
2024-08-29 16:41:07 +0200

    Add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { RouterView } from "vue-router";
 </script>
 
-<template>
-  <HelloWorld />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<template><RouterView /></template>
--- src/components/AddWineView.vue
+++ src/components/AddWineView.vue
@@ -4,11 +4,9 @@ import { ref, reactive } from "vue";
 
 const supabase = createSupabaseClient();
 const loading = ref(false);
-const user = ref<any>(null);
-const wineSearch = ref("");
-const wineSuggestions = ref<any[]>([]);
 const selectedWine = ref<any>(null);
 const isNewWine = ref(false);
+const imageUploadRef = ref<HTMLFormElement | null>(null);
 
 // Form state
 const formData = reactive({
@@ -18,64 +16,14 @@ const formData = reactive({
   price: null,
 });
 
-// const handleWineSearch = async (e: Event) => {
-//   const query = e.target.value;
-//   wineSearch.value = query;
-
-//   if (query.length > 2) {
-//     // Only search if the query is longer than 2 characters
-//     const { data, error } = await supabase
-//       .from("wines")
-//       .select("*")
-//       .ilike("wine_name", `%${query}%`)
-//       .limit(10);
-
-//     if (error) {
-//       console.error(error);
-//     } else {
-//       wineSuggestions.value = data;
-//     }
-//   } else {
-//     wineSuggestions.value = ["No wines matched your query"];
-//   }
-// };
-
-// const handleWineSelect = (wine: any) => {
-//   selectedWine.value = wine;
-//   Object.assign(formData, {
-//     wineName: wine.wine_name,
-//     year: wine.year,
-//     grape: wine.grape,
-//     price: wine.price,
-//   });
-//   wineSearch.value = wine.wine_name;
-//   wineSuggestions.value = [];
-//   isNewWine.value = false;
-// };
-
-// const handleNewWineToggle = (e: Event) => {
-//   selectedWine.value = null;
-//   Object.assign(formData, {
-//     wineName: "",
-//     year: null,
-//     grape: "",
-//     price: null,
-//   });
-//   wineSearch.value = "";
-
-//   isNewWine.value = e.target.checked;
-// };
-
 const handleSubmit = async (e: Event) => {
   e.preventDefault();
 
   loading.value = true;
 
-  const form = new FormData(e.currentTarget);
-
   const { wineName, year, grape, price } = formData;
-  const imageFile = form.get("image") as File;
 
+  const imageFile = imageUploadRef.value!.files[0] as File;
   const imageFileName = `${wineName}-${Date.now()}`;
 
   const { error: uploadDataError } = await supabase.storage
@@ -98,12 +46,12 @@ const handleSubmit = async (e: Event) => {
     return;
   }
 
-  let wineId = selectedWine ? selectedWine.id : null;
+  let wineId: number = selectedWine ? selectedWine.id : null;
 
   if (!wineId && isNewWine) {
     const { error: newWineError } = await supabase.from("wines").insert({
       wine_name: wineName,
-      year: parseInt(year),
+      year: parseInt(year!),
       grape: grape,
       price: price,
       image_url: imageUrlData?.publicUrl,
@@ -127,51 +75,6 @@ const handleSubmit = async (e: Event) => {
       @submit="handleSubmit"
       class="max-w-lg mx-6 p-4 bg-white shadow-md rounded-lg"
     >
-      <!-- <div v-if="!isNewWine" class="mb-4">
-        <label
-          htmlFor="wineSearch"
-          class="block text-sm font-medium text-gray-700"
-        >
-          Search Wine:
-        </label>
-        <input
-          type="text"
-          id="wineSearch"
-          name="wineSearch"
-          :value="wineSearch"
-          @change="handleWineSearch"
-          class="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        />
-
-        <ul
-          v-if="wineSuggestions.length > 0"
-          class="border border-gray-300 rounded-md mt-2 bg-white max-h-48 overflow-y-auto"
-        >
-          <li
-            v-for="wine in wineSuggestions"
-            :key="wine.id"
-            @click="() => handleWineSelect(wine)"
-            class="p-2 cursor-pointer hover:bg-indigo-100 truncate"
-          >
-            {{ wine.wine_name }} ({{ wine.year }})
-          </li>
-        </ul>
-      </div> -->
-
-      <!-- <div class="mb-4">
-        <label class="flex items-center">
-          <input
-            type="checkbox"
-            :checked="isNewWine"
-            @change="handleNewWineToggle"
-            class="form-checkbox"
-          />
-          <span class="ml-2 text-sm font-medium text-gray-700"
-            >Add New Wine</span
-          >
-        </label>
-      </div> -->
-
       <div class="mb-4">
         <label
           htmlFor="wineName"
@@ -223,6 +126,35 @@ const handleSubmit = async (e: Event) => {
       </div>
 
       <div class="mb-4">
+        <label htmlFor="grape" class="block text-sm font-medium text-gray-700">
+          Country:
+        </label>
+        <input
+          type="text"
+          id="grape"
+          name="grape"
+          v-model.lazy="formData.grape"
+          :disabled="!isNewWine && selectedWine"
+          class="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+      <div class="mb-4">
+        <label htmlFor="grape" class="block text-sm font-medium text-gray-700">
+          Region:
+        </label>
+        <input
+          type="text"
+          id="grape"
+          name="grape"
+          v-model.lazy="formData.grape"
+          :disabled="!isNewWine && selectedWine"
+          class="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+
+      <div class="mb-4">
         <label htmlFor="year" class="block text-sm font-medium text-gray-700">
           Price:
         </label>
@@ -250,6 +182,7 @@ const handleSubmit = async (e: Event) => {
           accept="image/*"
           class="mt-1 p-2 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           required
+          ref="imageUploadRef"
         />
       </div>
 
