25bbc4966e4a0e1e473c55c55cf4c5577aea2533
Jakob Punnerud <jepunnerud@gmail.com>
2024-09-03 00:14:47 +0200

    create views folder

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import StandardButton from "../components/StandardButton.vue"
+</script>
+<template>
+  <div class="bg-white min-h-screen flex justify-center content-center">
+    <div class="flex flex-col justify-center items-center fit-content py-8">
+      <h1 class="text-3xl font-bold mb-4">Velkommen til Stians vindatabase!</h1>
+      <p class="text-lg mb-8">Utforsk og ranger dine favorittviner.</p>
+      <div class="flex gap-5 w-full justify-center">
+        <StandardButton label="Se viner" />
+        <StandardButton href="/add-wine" label="Legg til ny vin" />
+      </div>
+    </div>
+  </div>
+</template>
